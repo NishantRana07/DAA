28cc882f846ea70542057bdc10976feb997c24a6
Nishant Rana <126577697+NishantRana07@users.noreply.github.com>
2024-11-03 12:25:43 +0000

    bubble sort program added

--- Sorting Algorithms/Bubble Sort/BubbleSort.cpp
+++ Sorting Algorithms/Bubble Sort/BubbleSort.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[5],i,j,temp;
+    //take input for the array
+    cout<<"Enter the 5 elemets in the array : \n";
+    for (i=0;i<5;i++)
+    {
+        cin>>a[i];
+    }
 
+    //logic
+
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<5-1;j++)
+        {
+            if(a[j+1]<a[j])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        } 
+    }
+
+    //Printing the output
+    cout<<"Sorted array : \n";
+    for (i=0;i<5;i++)
+    {
+        cout<<a[i]<<"\n";
+    }
+    return 0;
+}
